1784112ddf8e552f5c1299f76a9b70a6022676f5
salmanalmutairi <salman@psu.edu>
2023-10-22 12:05:26 -0400

    2

--- src/polaris-cta.js
+++ src/polaris-cta.js
@@ -8,7 +8,7 @@ class PolarisCta extends LitElement {
   static get styles() {
     return css`
       :host {
-        display: inline-block;
+        display: inline-flex;
         margin: 0 12px 12px 0;
       }
       
@@ -31,19 +31,54 @@ class PolarisCta extends LitElement {
         color: #444;
         font-size: 16px;
       }
+
+      .footer{
+        display: inline-flex;
+        border: 2px solid black;
+        padding: 2px;
+      }
+
+      .button {
+
+      }
+
+      p {
+        font-weight: 700;
+        display: inline-block;
+      }
+
+      a {
+        font-style: italic;
+        font-weight: 700;
+        letter-spacing: .025rem;
+        display: inline-block;
+        margin-left: 32px;
+      }
+
+      .wrapper {
+        height: 48px;
+        padding: 16px;
+        display: inline-block;
+        border: 2px solid black;
+        background-color: white;
+      }
     `;
   }
 
   constructor() {
     super();
+    this.
     this.header = 'My app';
   }
 
   render() {
     return html`
-      <div>
-        <p>test</p><a class="link" href="${this.link}"><slot>${this.name}</slot></a>
+      <div class="wrapper">
+        <p>GET THE NEWS BY EMAIL</p>
+        <a class="link" href="${this.link}"><slot>${this.name}</slot></a>
       </div>
     `;
   }
 }
+
+customElements.define('polaris-cta', PolarisCta);
