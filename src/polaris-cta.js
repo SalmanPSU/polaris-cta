677a4500254d104239aee9af5a9210dc561c71ea
salmanalmutairi <salman@psu.edu>
2023-10-23 20:11:45 -0400

    update

--- src/polaris-cta.js
+++ src/polaris-cta.js
@@ -3,14 +3,6 @@ import { LitElement, html, css } from 'lit';
 class PolarisCta extends LitElement {
   static get properties() {
     return {
-      name: {
-        type: String,
-      },
-
-      link: {
-        type: String,
-      },
-
       primaryOutlined: {
         type: Boolean,
         reflect: true
@@ -45,8 +37,8 @@ class PolarisCta extends LitElement {
         margin: 32px;
       }
 
-      /** From Midterm code by the professor - Dont use // for comments! */
-      /* .link:focus,
+      /** From Midterm code by the professor (Focus/Hover/Active) - Dont use // for comments! */
+      /** .link:focus,
       .link:hover,
       :host([active]) .link{
         background-color: #e4e5e7;
@@ -79,70 +71,65 @@ class PolarisCta extends LitElement {
         text-decoration: none;
         display: inline-block;
         margin-left: 32px;
+        padding: 12px 32px;
       }
 
       :host([primaryOutlined]) a {
         border: 2px solid #005fa9;
         background-color: #fff;
         color: #005fa9;
-        padding: 12px 32px;
       }
 
       :host([primaryFilled]) a {
         border: 2px solid #005fa9;
         background-color: #005fa9;
         color: #fff;
-        padding: 12px 32px;
       }
 
       :host([tinted]) a {
         border: 2px solid #fff;
         background-color: #4f627c;
         color: #b0e1ff;
-        padding: 12px 32px;
       }
 
-    :host([lightOutlined]) a {
+      :host([lightOutlined]) a {
       border: 2px solid #fff;
       background-color: #011e44;
       color: #a5e7fe;
-      padding: 12px 32px;
-    }
+      }
 
-    :host([lightFilled]) a {
+      :host([lightFilled]) a {
       border: 2px solid #a5e7fe;
       background-color: #a5e7fe;
       color: #011e44;
-      padding: 12px 32px;
-    }
+      }
 
       .wrapper {
         height: 48px;
         padding: 16px;
-        display: inline-block;
+        display: block;
         border: 2px solid black;
-        background-color: white;
       }
 
       :host([primaryOutlined]) .wrapper,
       :host([primaryFilled]) .wrapper {
       background-color: #fff;
-    }
+      }
 
       :host([tinted]) .wrapper {
       background-color: #e4e5e7;
-    }
+      }
 
       :host([lightOutlined]) .wrapper,
       :host([lightFilled]) .wrapper {
       background-color: #011e44;
-    }
+      }
     `;
   }
 
   constructor() {
     super();
-    this.name = 'Subscribe';
+    this.text = 'Subscribe';
     this.link = 'https://psu.edu';
   }
 
@@ -150,7 +137,7 @@ class PolarisCta extends LitElement {
     return html`
       <div class="wrapper">
         <p>GET THE NEWS BY EMAIL</p>
-        <a class="link" href="${this.link}"><slot>${this.name}</slot></a>
+        <a href="${this.link}" target="_blank"><slot>${this.text}</slot></a>
       </div>
     `;
   }
